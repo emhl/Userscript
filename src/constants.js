54e161aa9576906c21e52bf9170e8c0113521dba
Emhl <36165088+emhl@users.noreply.github.com>
2023-07-23 19:28:10 +0200

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const CHIEF_WS_ENDPOINT = ___WS_ENDPOINT___ || 'ws://chief.lan.steinr.eu:3000/ws';
+export const CHIEF_WS_ENDPOINT = ___WS_ENDPOINT___ || 'wss://chief.lan.steinr.eu/ws';
 export const USERSCRIPT_REVISION = ___GIT_HASH___ || 'development';
